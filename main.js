e179f1787ddde367eb211f73a52f2b223c823c4b
matt e <mattjescobar@gmail.com>
2018-10-19 00:34:40 -0700

    Webpage setup
    Fixed the JS calls from Abraham's to mine. Added the formating to CSS.

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
-console.log("hello")
-
 $.getJSON('https://api.airtable.com/v0/appRgPBG5cZYXh0C4/Recipes?api_key=keyLvHnHrkGTXgLbx',
 function(data){
     var html = [];
-    $.each(data.records, function(index, record) {
+    $.each(data.records, function(_index, record) {
       var name = record.fields['Name'];
-      var address = record.fields['Address'];
-      var rating = record.fields['Rating'];
-      html.push(`<h2>${name}, ${address}, ${rating}</h2>`);
+      var servings = record.fields['Servings'];
+      var prepTime = record.fields['Prep Time'];
+      html.push(`<h2>${name}</h2> <h3>${servings} servings, ${prepTime}</h3>`);
     });
     $('body').append(html);
   }
