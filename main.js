1f1f82982f60df89361a3d6c9fbca20f2f5886e0
mattjescobar <43800608+mattjescobar@users.noreply.github.com>
2018-10-30 18:42:36 -0700

    Coder wants to die
    WHYYYYYYYYYYYY

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 var button = function(name){
-  return `<button type="button" class="btn btn-secondary"><h2>${name}</h2></button>`;
+  return `<button type="button" class="btn btn-secondary btn-sm"><h6>${name}</h6></button>`;
 }; //Use the single tilde dash thing [`] (note it's sideways). Do not use single quote ['].
 
 $.getJSON("https://api.airtable.com/v0/appRgPBG5cZYXh0C4/Recipes?api_key=keyLvHnHrkGTXgLbx",
   function(data) {
       var html = [];
 
+      html.push(`<div class="row">`);
     $.each(data.records, function(_index, record) {
         var id = record.id;
         var fields = record.fields;
@@ -17,11 +18,15 @@ $.getJSON("https://api.airtable.com/v0/appRgPBG5cZYXh0C4/Recipes?api_key=keyLvHn
       var buttonName = button(name);
 
       html.push(
-        `<img src="${images}"> ${buttonName}<br>
-        <h3>${servings} servings, ${prepTime}</h3>`
+        `<div class="col-md-4"><img src="${images}"> <br>
+         ${buttonName}<br>
+        <h5>${servings} servings, ${prepTime}</h5></div>`
+        
       ); /* html.push is pushing the array titled html (defined in line 3). 'Push' pushes arrays. To have something push to somewhere else, I declare a new array (var mealtype = [];), fill that array ($.each(data.records, function(_index, record)) and then push to that array (mealtype.push('the rest of the commands'))*/
     });
-    $(".recipes").append(html); /* You're sending it to the '.recipes' class. You need the dot. */
+    console.log(html);
+   html.push('</div>') //don't have to push this, array will auto close. Why???
+    $(".recipes").append(html.join("")); /* You're sending it to the '.recipes' class. You need the dot. */
     }
 );
 
