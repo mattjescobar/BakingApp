3eb2c381d17ea3bbb9fa4a635b8a50d555a77b61
mattjescobar <43800608+mattjescobar@users.noreply.github.com>
2018-12-05 17:57:08 -0800

    Capping the T

--- main.js
+++ main.js
@@ -38,7 +38,7 @@ var navbar = function (){
 var jumbotron = function() {
   return `<div class="jumbotron jumbotron-fluid">
   <div class="container">
-    <h1 class="display-4">Set the Table</h1>
+    <h1 class="display-4">Set The Table</h1>
     <p class="lead">what are you hungry for today?</p>
   </div>
 </div>`;
